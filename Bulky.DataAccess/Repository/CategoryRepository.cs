2e4c1325546d891a25cc4bae43da582fce5f857c
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2023-09-10 23:24:41 +0700

    [U] Repository Pattern

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Repository;
+using Bulky.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}

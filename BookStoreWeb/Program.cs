25bc21bff7c44b3e39580358e8b6b89863704244
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2023-09-10 09:42:49 +0700

    N-Tier Architecture

--- BookStoreWeb/Program.cs
+++ BookStoreWeb/Program.cs
@@ -1,4 +1,4 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
